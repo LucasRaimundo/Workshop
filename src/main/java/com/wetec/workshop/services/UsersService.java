ce1c817259a19e6df0fad392c437d2b5557a575b
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-08-26 12:08:36 -0300

    Service layer, component registration

---
+++ src/main/java/com/wetec/workshop/services/UsersService.java
@@ -0,0 +1,26 @@
+package com.wetec.workshop.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wetec.workshop.entities.Users;
+import com.wetec.workshop.repositories.UsersRepository;
+
+@Service
+public class UsersService {
+
+	@Autowired
+	private UsersRepository repository;
+	
+	public List<Users> findAll(){
+		return repository.findAll();
+	}
+	
+	public Users findById(Long id){
+		Optional<Users> obj = repository.findById(id);
+		return obj.get();
+	}
+}
